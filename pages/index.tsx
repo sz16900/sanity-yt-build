d22eb6f8ae33264722331a44d3249c9d30fedcfb
Seth Zea <sz16900@my.bristol.ac.uk>
2022-12-30 12:35:42 -1000

    blogs component

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ interface Props {
 // unlike in the video, this works... seems like props can be passed into after NextPage
 // and now it works with <Props>? must be typescript thing
 const Home: NextPage<Props> = ({posts}: Props) => {
-  console.log(posts);
+  console.log();
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -26,15 +26,24 @@ const Home: NextPage<Props> = ({posts}: Props) => {
 
       <Banner />
 
-      <div>
+      {/* Posts */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3
+      md:gap-6 p-2 lg:p-6">
         {posts.map(post => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
-              {/* only render this if there is a main image present */}
-              {post.mainImage && (
-                // exclamation mark to ensure it is not null?
-                <img src={urlFor(post.mainImage).url()!} alt="" />
-              )}
+            <div className="group border rounded-lg overflow-hidden">
+                {/* only render this if there is a main image present */}
+                {post.mainImage && (
+                  // exclamation mark to ensure it is not null?
+                  <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt="" />
+                )}
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">{post.description} by {post.author.name}</p>
+                </div>
+                <img className="h-12 w-12 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
+              </div>
             </div>
           </Link>
         ))}
